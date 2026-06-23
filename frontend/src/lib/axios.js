86776b433569d2118e928d84a29cb1299a09d669
son309 <giangson309@gmail.com>
2026-06-23 14:38:11 +0700

    prepare frontend for vervel deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:3000/api" : "/api",
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:3000/api"
+      : import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
 
